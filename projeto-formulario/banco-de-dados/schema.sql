26b7408792b45436c6c2863e23b20124b3a4a9a3
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2024-02-24 14:53:31 -0300

    Update schema.sql

--- projeto-formulario/banco-de-dados/schema.sql
+++ projeto-formulario/banco-de-dados/schema.sql
@@ -1,11 +1,12 @@
-CREATE DATABASE phpdocker;
+CREATE DATABASE IF NOT EXISTS banco_de_dados;  /**cria o banco de dados **/ 
+USE banco_de_dados;    /**usa o banco de dados **/
 
-CREATE TABLE IF NOT EXISTS cliente (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(50),
-    cpf CHAR(14),
-    email VARCHAR(50),
-    hash_senha VARCHAR(225),
-    data_nascimento DATE,
-    estado_civil VARCHAR(30),
+CREATE TABLE IF NOT EXISTS `cliente`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nome` VARCHAR(50),
+    `cpf` CHAR(14),
+    `email` VARCHAR(50),
+    `hash_senha` VARCHAR(225),
+    `data_nascimento` DATE,
+    `estado_civil` VARCHAR(30)
 );
